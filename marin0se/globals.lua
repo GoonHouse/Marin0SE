d02a324c5a148a89699e6dad16f6c7a17138b997
EntranceJew <EntranceJew@gmail.com>
2014-10-14 12:52:11 -0400

    Added sound search directories, although currently not used. Renamed "nographic" to the more accurate "missinggraphic". Added "missingsound" and I hope nobody ever has to hear it.

--- marin0se/globals.lua
+++ marin0se/globals.lua
@@ -2,13 +2,22 @@
 	The purpose of this is to document / list variables in the global scope.
 ]]
 
+soundsearchdirs = {
+	"mappacks/%(mappack)s/sounds/%(soundpack)s/%(file)s",
+	"mappacks/%(mappack)s/sounds/DEFAULT/%(file)s",
+	"mappacks/%(mappack)s/music/%(file)s",
+	"sounds/%(soundpack)s/%(file)s",
+	"sounds/DEFAULT/%(file)s",
+	"sounds/DEFAULT/missingsound.ogg"
+}
+
 graphicssearchdirs = {
 	"mappacks/%(mappack)s/graphics/%(graphicspack)s/%(file)s",
 	"mappacks/%(mappack)s/graphics/DEFAULT/%(file)s",
 	"mappacks/%(mappack)s/graphics/%(file)s",
 	"graphics/%(graphicspack)s/%(file)s",
 	"graphics/DEFAULT/%(file)s",
-	"graphics/DEFAULT/nographic.png"
+	"graphics/DEFAULT/missinggraphic.png"
 }
 
 enemygraphicsearchdirs = {
