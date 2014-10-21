41621097e2e1d4ac2e659b98fcf24ca8c5942dd5
HammerGuy <hammyhammerguy@gmail.com>
2014-10-21 11:52:49 -0400

    Unique firework graphic

--- marin0se/entities/firework.lua
+++ marin0se/entities/firework.lua
@@ -2,7 +2,7 @@ firework = class("firework")
 
 function firework:init(x, yoffset)
 	self.x = x+(math.random(9)-5)
-	self.y = math.random(5)+2+yoffset
+	self.y = math.random(5)+3+yoffset
 	self.timer = 0
 	marioscore = marioscore + 200
 end
@@ -17,17 +17,27 @@ function firework:update(dt)
 	if self.timer > fireworkdelay then
 		return true
 	end
+	
+	if self.destroy then
+	return true
+	end
 end
 
 function firework:draw()
 	local framelength = fireworkdelay/3
-	local frame = 5
-	if self.timer > framelength then
-		frame = 6
+	local frame = 1
+	if self.timer > framelength*1.1 then
+		frame = 2
 	end
-	if self.timer > framelength*2 then
-		frame = 7
+	if self.timer > framelength*1.25 then
+		frame = 3
 	end
-	
-	love.graphics.draw(fireballimg, fireballquad[frame], math.floor((self.x-xscroll)*16*scale), (self.y-yscroll-0.5)*16*scale, 0, scale, scale, 8, 8)
+	if self.timer > framelength*1.5 then
+		frame = 4
+	end
+	love.graphics.draw(fireworkimg, fireworkquads[frame], math.floor((self.x-xscroll)*16*scale), (self.y-yscroll-0.5)*16*scale, 0, scale, scale, 16, 16)
+	if self.timer > framelength*1.7 then
+		self.destroy=true
+		return true
+	end
 end
