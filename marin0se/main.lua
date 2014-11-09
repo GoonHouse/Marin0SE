933e804bf1746799346ca9de2df065c00d1d83d0
bungalox <captainbungalo@gmail.com>
2014-11-08 20:56:40 -0500

    Removed Enemy - "sunfire" and added Enemy - "fireball" (sunfire would not display proper animations)
    Removed Enemy - drybones (forgot to put him back in while searching for an error)
    Added boss_spit and enemy_hit to main.lua and sounds
    Tweaked clyde

--- marin0se/main.lua
+++ marin0se/main.lua
@@ -2230,7 +2230,7 @@ function reloadGraphics()
 end
 
 function reloadSounds() -- mastersfx, master list of sounds current being looked at.
-	soundstoload = {"none", "jump", "jumpbig", "stomp", "shot", "blockhit", "blockbreak", "coin", "pipe", "boom", "mushroomappear", "mushroomeat", "shrink", "death", "gameover", "fireball", "redcoin1", "redcoin2", "redcoin3", "redcoin4", "redcoin5",
+	soundstoload = {"none", "jump", "jumpbig", "stomp", "shot", "blockhit", "blockbreak", "coin", "pipe", "boom", "mushroomappear", "mushroomeat", "shrink", "death", "gameover", "fireball", "redcoin1", "redcoin2", "redcoin3", "redcoin4", "redcoin5", "boss_spit", "enemy_hit",
 					"oneup", "levelend", "castleend", "scorering", "intermission", "fire", "bridgebreak", "bowserfall", "vine", "swim", "rainboom", "konami", "pause", "bulletbill", "addtime", "throw", "trophy", "switch",
 					"lowtime", "tailwag", "planemode", "stab", "spring", "portal1open", "portal2open", "portalenter", "portalfizzle"}
 				
