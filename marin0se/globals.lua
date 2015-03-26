cc84bd7d886d4eec00a8acfa263041f20bdd5e37
HammerGuy <hammyhammerguy@gmail.com>
2015-03-26 10:16:08 -0400

    Graphic Reorganization:
    - Most of the HUD elements moved/rearranged to its own folder.
    - Vast majority of the Mario Graphics setup for the new animation system
    - Also a few alternate powerup graphics, however not setup to appear in anyways at the moment.
    - MARIN0 graphics folder removed as it served its purpose at the time, testing graphic folders.
    
    (cherry picked from commit ad5e623588ebb1cf94b3eeac82b413b3683f313f)
    
    Conflicts:
    	marin0se/graphics/DEFAULT/guihud/nographic.png
    	marin0se/main.lua

--- marin0se/globals.lua
+++ marin0se/globals.lua
@@ -25,7 +25,7 @@ graphicssearchdirs = {
 	"mappacks/%(mappack)s/graphics/%(file)s",
 	"graphics/%(graphicspack)s/%(file)s",
 	"graphics/DEFAULT/%(file)s",
-	"graphics/DEFAULT/missinggraphic.png"
+	"graphics/DEFAULT/guihud/missinggraphic.png"
 }
 
 enemygraphicsearchdirs = {
